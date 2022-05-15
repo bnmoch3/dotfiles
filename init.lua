49949978d691690ecbaecef8dfd2c623748f95e7
nm <nagamocha@gmail.com>
2022-05-15 23:42:46 +0300

    Configure LSP

--- init.lua
+++ init.lua
@@ -48,6 +48,7 @@ require("packer").startup(function(use)
 	use("nvim-lua/plenary.nvim")
 	use("DanilaMihailov/beacon.nvim")
 	use("nvim-treesitter/nvim-treesitter")
+	use("williamboman/nvim-lsp-installer")
 	use("neovim/nvim-lspconfig")
 
 	-- themes
@@ -286,3 +287,221 @@ nnoremap("<leader>fm", "<cmd>lua require('telescope.builtin').marks()<cr>")
 nnoremap("<leader>fr", "<cmd>lua require('telescope.builtin').registers()<cr>")
 nnoremap("<leader>fl", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>")
 nnoremap("<leader>fd", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>")
+
+-- ============================================================================
+--                              LSP
+-- ============================================================================
+
+--[[
+Note:
+    taplo for toml
+Other LSPs to keep in mind for future
+    Web-Dev: 
+        cssls, html, vuels, cssls, tailwindcss, jsonls, 
+        cssmodules_ls, graphql, eslint
+    Sql: 
+        sqls, sqlls
+    Markdown: 
+        prosemd_lsp, remark_ls, za
+    Misc: 
+        asm_lsp(Assembly), cmake, bashls, zls(Zig)
+        sorbet(Ruby), solargraph(Ruby), elixirls
+        tflint(Terraform), terraformls(Terraform),
+        rust_analyzer, ltex(Latex)
+
+- stop all clients, then reload the buffer
+    :lua vim.lsp.stop_client(vim.lsp.get_active_clients())
+
+- default handlers used when creating a new client:
+    :lua print(vim.inspect(vim.tbl_keys(vim.lsp.handlers)))
+--]]
+
+local lsp_actions = {
+	{ -- resolve document highlights for current text document pos.
+		cmd = "document_highlight",
+		keybinding = nil,
+		action = vim.lsp.buf.document_highlight,
+	},
+	{ -- lists all symbols in the current buffer
+		cmd = "document_symbol",
+		keybinding = nil,
+		action = vim.lsp.buf.document_symbol,
+	},
+	{ -- retrieves completion items at curr cursor pos, can only be used in insert mode
+		cmd = "complete",
+		keybinding = nil,
+		action = vim.lsp.buf.completion,
+	},
+	{ -- jumps to the declaration of the sym under the cursor, not impl by many lsps
+		cmd = "declaration",
+		keybinding = nil,
+		action = vim.lsp.buf.declaration,
+	},
+	{ -- jumps to the def of symbols under the cursor
+		cmd = "definition",
+		keybinding = {"<Leader>gd","<C-]>"},
+		action = vim.lsp.buf.definition,
+	},
+	{ -- displays information about the symbol under the cursor
+		cmd = "hover",
+		keybinding = "<Leader>gh",
+		action = vim.lsp.buf.hover,
+	},
+	{ -- lists all the implementations for the symbol under the cursor
+		cmd = "implementation",
+		keybinding = nil,
+		action = vim.lsp.buf.implementation,
+	},
+	{ -- lists all the call sites of the sym under the cursor
+		cmd = {"called_by", "incoming"},
+		keybinding = nil,
+		action = vim.lsp.buf.incoming_calls,
+	},
+	{ -- list all items that are called by the symbol under the cursor
+		cmd = {"calls", "outgoing"},
+		keybinding = nil,
+		action = vim.lsp.buf.outgoing_calls,
+	},
+	{ -- lists all the references to the symbol under the cursor in the quickfix window
+		cmd = "references",
+		keybinding = nil,
+		action = vim.lsp.buf.references,
+	},
+	{ -- renames all the references to the symbol under the cursor
+		cmd = "rename",
+		keybinding = nil,
+		action = vim.lsp.buf.rename,
+	},
+	{ -- displays signature information for sym under the cursor
+		cmd = "signature_help",
+		keybinding = nil,
+		action = vim.lsp.buf.signature_help,
+	},
+	{ -- jumps to the definition of the type of sym under the cursor
+		cmd = "type_definition",
+		keybinding = "<Leader>gt",
+		action = vim.lsp.buf.type_definition,
+	},
+	{ -- removes document highlights from current buffer
+		cmd = "clear_references",
+		keybinding = nil,
+		action = vim.lsp.buf.clear_references,
+	},
+}
+
+local custom_lsp_attach = function(client)
+	local function pluck_first(cmd)
+		if type(cmd) == "string" then
+			return cmd
+		end
+		if type(cmd) == "table" then
+			return cmd[1]
+		end
+	end
+    
+    local function flatten_apply(val, fn)
+        if val == nil then
+            return
+        end
+        if type(val) == "table" then
+            for _,v in ipairs(val) do
+                fn(v)
+            end
+            return
+        end
+        fn(val)
+    end
+
+	-- set up keyboard shortcuts
+	for _, v in ipairs(lsp_actions) do
+        flatten_apply(
+            v.keybinding,
+            function (keybinding)
+                if keybinding ~= nil and keybinding ~= "" then
+                    local cmd = pluck_first(v.cmd)
+                    nnoremap(keybinding, "<cmd>Lsp " .. cmd .. "<cr>")
+                end
+            end
+        )
+	end
+
+	-- tab complete for Lsp command
+	function complete_lsp_command(ArgLeader)
+		local cmds = {}
+		for _, v in ipairs(lsp_actions) do
+            flatten_apply(
+                v.cmd,
+                function(cmd)
+                    if string.find(cmd, "^" .. ArgLeader) ~= nil then
+                        cmds[#cmds + 1] = cmd 
+                    end
+                end
+            )
+		end
+        pp(cmds)
+		return cmds
+	end
+
+	-- set up Lsp command
+	vim.api.nvim_create_user_command(
+		"Lsp",
+		(function()
+			-- build reverse map from cmd to actions
+			local cmds_to_actions = {}
+			for _, v in ipairs(lsp_actions) do
+                flatten_apply(
+                    v.cmd,
+                    function(cmd) 
+                        cmds_to_actions[cmd] = v.action 
+                    end
+                )
+			end
+			-- return closure to access reverse map
+			return function(opts)
+				local sub_cmd = opts.args
+				local action_fn = cmds_to_actions[sub_cmd]
+				if action_fn == nil then
+					error("Invalid cmd: " .. sub_cmd)
+					return
+				end
+				action_fn()
+			end
+		end)(),
+		{
+			nargs = 1,
+			complete = complete_lsp_command,
+		}
+	)
+end
+
+require("nvim-lsp-installer").setup({})
+local lspconfig = require("lspconfig")
+local lang_servers = {
+	pyright = {},
+	gopls = {},
+	clangd = {},
+	dockerls = {},
+	tsserver = {},
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				diagnostics = { globals = { "vim" } },
+				disable = { "lowercase-global" },
+				workspace = {
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+					},
+				},
+			},
+		},
+	},
+	vimls = {},
+	taplo = {},
+	yamlls = {},
+}
+
+for lang_server, config in pairs(lang_servers) do
+	config.on_attach = custom_lsp_attach
+	lspconfig[lang_server].setup(config)
+end
