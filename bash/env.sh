12cee1c12cb8bafb48af4aa128c75f9d6a675b88
nm <nagamocha@gmail.com>
2022-05-31 22:20:24 +0300

    Fix coloring for vim scheme

--- bash/env.sh
+++ bash/env.sh
@@ -13,10 +13,6 @@ if command -v bat >/dev/null; then
 	export PAGER=bat
 fi
 
-if [[ -z "$TMUX" ]]; then
-	export TERM=xterm-256color
-fi
-
 # FZF options
 if type rg &>/dev/null; then
 	export FZF_DEFAULT_COMMANND='rg --files'
@@ -34,19 +30,19 @@ fi
 # go
 if [[ -d /usr/local/go ]]; then
 	PATH=$PATH:/usr/local/go/bin
-    PATH=$PATH:$(go env GOPATH)/bin
+	PATH=$PATH:$(go env GOPATH)/bin
 fi
 
 # pyenv
 if [[ -d "$HOME/.pyenv" ]]; then
 	export PYENV_ROOT="$HOME/.pyenv"
-    PATH=$PATH:$PYENV_ROOT/bin
+	PATH=$PATH:$PYENV_ROOT/bin
 	eval "$(pyenv init -)"
 fi
 
 # PATH="$PATH:/home/bnm/installed/bin:/home/bnm/installed/bin/SQLiteStudio/"
 if [[ -d "$HOME/dotfiles" ]]; then
-    PATH="$HOME/bin:$PATH"
+	PATH="$HOME/bin:$PATH"
 fi
 
 export PATH
