505cf1936a2fe940bffd563cc0f7d0ba925c7df1
nm <nagamocha@gmail.com>
2022-05-31 20:00:04 +0300

    Remove aliases that I haven't been using a lot

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -5,12 +5,6 @@
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
 fi
 
 # soft delete for files
@@ -19,19 +13,10 @@ alias trash='trash-put'
 
 # some more ls aliases
 alias ll='ls -alF'
-alias ls1='ls -1'
-alias la='ls -A'
-alias l='ls -CF'
+alias l1='ls -1'
 
 alias pwdd='pwd | xclip -i'
 alias cwdd='cd $(xclip -o)'
-
-alias hackertools='cd /home/bnm/PROJECTS/hackertools'
-alias projects='cd /home/bnm/PROJECTS/'
-
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias activate="source .venv/bin/activate"
 
