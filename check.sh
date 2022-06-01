171181527d24206677c447b960548f45681d6d70
nm <nagamocha@gmail.com>
2022-06-01 19:02:09 +0300

    Format list of cli tools

--- check.sh
+++ check.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 
 _check_cmds() {
-	local check_must_install=(tmux nvim go node npm yarn cargo
-		rg fzf patdiff bat shellcheck delta)
-	local check_nice_to_have=(pyenv goimports prettier eslint shfmt exa)
+	local check_must_install=(
+		tmux
+		nvim
+		go
+		node
+		npm
+		yarn
+		cargo
+		rg
+		fzf
+		patdiff
+		bat
+		shellcheck
+		delta
+	)
+	local check_nice_to_have=(
+		pyenv
+		goimports
+		prettier
+		eslint
+		shfmt
+		exa
+	)
 
 	local installed=()
 	local must_install=()
