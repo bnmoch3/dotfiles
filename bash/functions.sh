d6f20339294b0f18db445d1347b72431f9aaa3e4
nm <nagamocha@gmail.com>
2022-07-16 23:59:31 +0300

    Add nightmode/daymode helpers

--- bash/functions.sh
+++ bash/functions.sh
@@ -111,3 +111,17 @@ function find1() {
 	fi
 	find $dir -maxdepth 1 "$@"
 }
+
+# nightmode
+function nightmode() {
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		redshift -x
+		redshift -o -O 1500
+	fi
+}
+
+function daymode() {
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		redshift -x
+	fi
+}
