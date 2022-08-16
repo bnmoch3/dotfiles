1d167f30191f41edb63f83dfc6ca8106df1cf1f4
nm <nagamocha@gmail.com>
2022-08-16 09:24:24 +0300

    Set up format for markdown on insert leave

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,3 +1,14 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
+
+local function on_leave_insert_md()
+	vim.lsp.buf.format()
+	vim.cmd("normal ze")
+end
+local fmtMarkdownGroup = vim.api.nvim_create_augroup("FmtMarkdownGroup", { clear = true })
+vim.api.nvim_create_autocmd("InsertLeave", {
+	callback = on_leave_insert_md,
+	pattern = { "*.md" },
+	group = fmtMarkdownGroup,
+})
