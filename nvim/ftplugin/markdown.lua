39e96c7e989924e3fd57634b9345fffc8aedc9a3
nm <nagamocha@gmail.com>
2022-09-21 19:16:13 +0300

    Rename function for formatting markdown

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -2,13 +2,14 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 
-local function on_leave_insert_md()
+local function format_markdown()
 	vim.lsp.buf.format()
 	vim.cmd("normal ze")
 end
+
 local fmtMarkdownGroup = vim.api.nvim_create_augroup("FmtMarkdownGroup", { clear = true })
 vim.api.nvim_create_autocmd("InsertLeave", {
-	callback = on_leave_insert_md,
+	callback = format_markdown,
 	pattern = { "*.md" },
 	group = fmtMarkdownGroup,
 })
