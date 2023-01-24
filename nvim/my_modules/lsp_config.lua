4ae997b402097b9cbb08014c2d5c632ce007b8af
nm <nagamocha@gmail.com>
2023-01-24 14:03:30 +0300

    Add line-length for formatting with black

--- nvim/my_modules/lsp_config.lua
+++ nvim/my_modules/lsp_config.lua
@@ -284,7 +284,8 @@ local function setup_null_ls()
 			null_ls.builtins.formatting.taplo,
 			-- python
 			null_ls.builtins.formatting.black.with({
-				command = vim.fn.expand("~/LOCAL/dev/python/venv/bin/black"),
+				-- command = vim.fn.expand("~/LOCAL/dev/python/venv/bin/black"),
+				extra_args = { "--line-length", "80" },
 			}),
 			-- markdown
 			null_ls.builtins.formatting.deno_fmt.with({
