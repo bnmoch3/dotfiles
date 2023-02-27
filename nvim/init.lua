a39bc78b3fd20d61281703b7cb03ae2f593ab649
nm <nagamocha@gmail.com>
2023-02-27 21:08:56 +0300

    Use configurable base-16 colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -62,8 +62,8 @@ require("packer").startup(function(use)
 	use("rafamadriz/friendly-snippets")
 
 	-- themes, styling
-	use("ellisonleao/gruvbox.nvim")
-	use("chriskempson/base16-vim")
+	-- use("chriskempson/base16-vim")
+	use("RRethy/nvim-base16")
 	use("DanilaMihailov/beacon.nvim") -- temporarily highlight cursor's curr line
 	use("lukas-reineke/indent-blankline.nvim") -- indentation guides
 
@@ -149,7 +149,25 @@ vim.o.smartcase = true
 -- TODO install filetype
 -- vim.g.did_load_filetypes = 1
 
-vim.cmd([[colorscheme base16-gruvbox-dark-pale]])
+require("base16-colorscheme").setup({
+	base00 = "#262626",
+	base01 = "#3a3a3a",
+	base02 = "#4e4e4e",
+	base03 = "#8a8a8a",
+	base04 = "#949494",
+	base05 = "#dab997",
+	base06 = "#d5c4a1",
+	base07 = "#ebdbb2",
+	base08 = "#d75f5f",
+	base09 = "#ff8700",
+	base0A = "#ffaf00",
+	base0B = "#afaf00",
+	base0C = "#85ad85",
+	base0D = "#83adad",
+	base0E = "#d485ad",
+	base0F = "#d65d0e",
+})
+-- vim.cmd("colorscheme base16-gruvbox-dark-pale")
 vim.o.termguicolors = true
 
 vim.g.beacon_size = 60
