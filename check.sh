43760f04a764f38140782a8532e97f6e4da85f61
nm <nagamocha@gmail.com>
2023-02-28 18:41:03 +0300

    Remove delta for git diffs

--- check.sh
+++ check.sh
@@ -13,7 +13,6 @@ _check_cmds() {
 		fzf        # fuzzy finder
 		bat        # cat alternative
 		shellcheck # lint shell scripts
-		delta      # diff alternative
 		nvm        # for node version mgmt
 	)
 	local check_nice_to_have=(
